fdb4ad9df5b2dbbe5cad8973b81a98b61c2785d5
Konstantin Gavras <konstantin@gavras.de>
2018-01-28 22:52:28 -0500

    Add files via upload

---
+++ resources/left/left2.py
@@ -0,0 +1,42 @@
+import lxml
+import requests
+import urllib2
+import re
+import os
+
+from lxml import html
+from bs4 import BeautifulSoup
+
+#Set content, which is required in the url:
+contentInUrl = ["presse/pressemitteilungen/detail/"]
+
+#contentnotInUrl = ["/seite/"]
+#Extract only those URLs we are interested in:
+with open ("urls/left/left.txt", "r") as myfile:
+    data = myfile.read().split("\n")
+
+#print data
+
+data_correct = []
+for dat in data:
+    if any(word in dat for word in contentInUrl):
+        dat = "https://www.linksfraktion.de/" + dat
+        data_correct.append(dat)
+        #print data_correct
+
+
+seen = set()
+result = []
+for item in data_correct:
+    if item not in seen:
+        seen.add(item)
+        result.append(item)
+
+#print result
+
+#del result[0]
+
+newfile = open("urls/left/left_korrekt.txt", "w")
+for ele in result:
+    newfile.write(ele+'\n')
+newfile.close()
